322aa289a806d5e685ba3dfc378901fbb2983777
Tuco2006 <mateussmcruz@icloud.com>
2026-05-15 14:05:24 -0300

    fix: routes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/MachinePay.github.io/', // 👈 coloque exatamente o nome do seu repositório aqui
+  base: '/',
 })
